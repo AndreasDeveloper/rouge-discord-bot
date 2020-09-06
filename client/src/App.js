b4d1aaea54730b4c93b1812fe6983021fc9b9aa7
AndreasDEV <andreasmrdja16@gmail.com>
2020-09-06 02:10:17 +0200

    Integrated React for client side dashboard.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Button } from '@chakra-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variantColor="orange">button</Button>
+    </div>
+  );
+}
+
+export default App;
